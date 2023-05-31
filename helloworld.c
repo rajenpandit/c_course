fc9209e9ca18fb732541784faa8866bc882b669d
Rajendra Pandit <rajenpandit@gmail.com>
2023-05-31 21:06:17 +0530

    compiler options discussion

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include "sum.h"
+
+#define ABC 10
+
+// libc.so 
+// source code -> Preprocessor -> compiler -> ObjectCode -> Linker -> Executable
+
+int main() {
+
+    int s = sum(2, ABC);
+   return 0;
+}
---
+++ helloworld.cc
@@ -0,0 +1,24 @@
+# 1 "helloworld.c"
+# 1 "<built-in>" 1
+# 1 "<built-in>" 3
+# 400 "<built-in>" 3
+# 1 "<command line>" 1
+# 1 "<built-in>" 2
+# 1 "helloworld.c" 2
+# 1 "./sum.h" 1
+# 1 "./math.h" 1
+int sub(int a, int b);
+# 2 "./sum.h" 2
+int sum(int, int);
+# 2 "helloworld.c" 2
+
+
+
+
+
+
+int main() {
+
+    int s = sum(2, 10);
+   return 0;
+}
