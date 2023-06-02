f3e901c36100a60348b6a3d7736df996c7ec298b
Rajendra Pandit <rajenpandit@gmail.com>
2023-06-02 20:44:07 +0530

    array

---
+++ dataTypes/array.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+// store 'Hello World' into an array.
+//'\0' => 00000000
+// [H|e|l|l|o| |W|o|r|l|d|\0]
+//100 1 2 3 4 5 6 7 8 9 10 11
+// p=103
+
+void print(char* p) {
+    while (*p != '\0')
+    {
+        printf("%c",*p);
+        p++; // p=p+1
+    }
+}
+
+void copy(char* dst, const char* src) {
+
+    while(*src != '\0') {
+        *dst = *src;
+        dst++;
+        src++;
+    }
+}
+
+int main() {
+    // char arr[12];
+    // arr[0] = 'H';
+    // arr[1] = 'e';
+    // arr[2] = 'l';
+    // arr[3] = '\0';
+
+    printf("Helloworld!!!\nsomething..\n X=%c Y=%d jsjdfjl ajs",66, 67); //%c, %d, %f, %s, %ul, %p
+    printf("\n");
+    char arr[] = "Hello World";
+    char str[100];
+
+    // strcpy(str, "Test string");
+    copy(str, "Test string");
+    printf("%s\n", str);
+    printf("%s\n",arr);
+
+    int i=0;
+    print(arr);
+
+    printf("\n");
+
+    
+}
+
+
+
